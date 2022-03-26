9c19366d973886303421a07066d98350985c4792
raaidkabir <kabiraaid@gmail.com>
2022-03-26 16:54:59 -0400

    added chat and sms verification

---
+++ components/CustomListItem.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { ListItem,Avatar } from 'react-native-elements'
+import { db } from '../firebase'
+
+const CustomListItem = ({id, chatName,enterChat}) => {
+    const [chatMessages, setChatMessages] = useState([])
+    useEffect(() => {
+        const unsubscribe = db
+        .collection('chats')
+        .doc(id)
+        .collection('messages')
+        .orderBy('timestamp', 'desc')
+        .onSnapshot((snapshot)=>
+        setChatMessages(snapshot.docs.map((doc)=>doc.data()))
+        )
+        return unsubscribe
+    }, [])
+    return (
+        <ListItem key={id} bottomDivider onPress={()=>enterChat(id, chatName)}>
+            <Avatar 
+            rounded
+            source={{uri: chatMessages?.[0]?.photoURL||'https://www.kindpng.com/picc/m/22-223941_transparent-avatar-png-male-avatar-icon-transparent-png.png'}}
+            />
+            <ListItem.Content>
+                <ListItem.Title>
+                    {chatName}
+                </ListItem.Title>
+                <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
+                    {chatMessages?.[0]?.displayName}: {chatMessages?.[0]?.message}
+                </ListItem.Subtitle>
+            </ListItem.Content>
+        </ListItem>
+    )
+}
+
+export default CustomListItem
+
+const styles = StyleSheet.create({})
---
+++ screens/chatstack/MainChatScreen.js
@@ -0,0 +1,75 @@
+import React, {useLayoutEffect,useState,useEffect} from 'react'
+import { StyleSheet, Text, View,SafeAreaView,ScrollView, TouchableOpacity} from 'react-native'
+import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
+import CustomListItem from '../../components/CustomListItem'
+import { auth,db } from '../../firebase'
+import {AntDesign, SimpleLineIcons} from 'react-native-vector-icons'
+
+const MainChatScreen = ({navigation}) => {
+    const [chats,setChats] = useState([])
+    useEffect(() => {
+        const unsubscribe = db.collection('chats').onSnapshot((snapshot)=>
+        setChats(
+            snapshot.docs.map((doc)=>({
+                id: doc.id,
+                data: doc.data(),
+            }))
+        ))
+        return unsubscribe
+    }, [])
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            title: 'Iris',
+            headerStyle: {backgroundColor: 'white'},
+            headerTitleStyle: {color: 'black'},
+            headerTintColor: 'black',
+            headerLeft: ()=>(
+                <View style={{marginLeft: 20}}>
+                    <Avatar rounded source={{uri: auth?.currentUser?.photoURL}}/>
+                </View>
+                ),
+            headerRight: ()=>(
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    width:80,
+                    marginRight:20
+                }}>
+                    <TouchableOpacity activeOpacity={0.5}>
+                        <AntDesign name='camerao' size={24} color='black'/>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={0.5} onPress={()=>(
+                        navigation.navigate('AddChat')
+                    )} >
+                        <SimpleLineIcons name='pencil' size ={24} color='black'/>
+                    </TouchableOpacity>
+                </View>
+            )
+
+        })
+    })
+    const enterChat = (id,chatName)=>{
+        navigation.navigate('Chat',
+        {
+            id,
+            chatName,
+        })
+    }
+    return (
+        <SafeAreaView>
+            <ScrollView style={styles.container}>
+            {chats.map(({id,data:{chatName}})=>(
+                <CustomListItem key={id} id={id} chatName = {chatName}  enterChat = {enterChat}/>
+            ))}
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default MainChatScreen
+
+const styles = StyleSheet.create({
+    container:{
+        height: '100%'
+    }
+})
